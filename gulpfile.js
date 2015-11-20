b41b1891a46472f422c6d61a9759a607c9dc3522
Gianpaulo <gsoares@uolinc.com>
2015-11-20 15:19:49 -0200

    Colocando a lib na pasta lib

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,26 @@ var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var mocha = require('gulp-mocha');
 
-gulp.task('default', ['build','test-build'], function(){
-	console.log('done');
-});
+var lib = 'lib/azubu-api.js';
+
+gulp.task('default', ['build']);
 
 gulp.task('watch', function(){
-	watch(['test/**/*.js','azubu-api.js'], batch(function(events, done) {
+	watch(['test/**/*.js', lib], batch(function(events, done) {
 		gulp.start('test', done);
 	}));
 });
 
 gulp.task('build', function(){
-	return gulp.src('azubu-api.js')
+	return gulp.src(lib)
 		.pipe(replace(/\/\*\* Test-begins[.\n\N\s\S]*?Test-ends \*\*\//g, ''))
+		.pipe(uglify())
 		.pipe(rename('azubu-api.min.js'))
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('test-build', function(){
-	return gulp.src('azubu-api.js')
+	return gulp.src(lib)
 		.pipe(uglify())
 		.pipe(rename('azubu-api.test.min.js'))
 		.pipe(gulp.dest('dist'));
